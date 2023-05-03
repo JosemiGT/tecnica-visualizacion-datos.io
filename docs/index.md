401346c1fcffe038e019b619ae3775c9afc00fed
JosemiGT <jose.gamarro.tornay@avanade.com>
2023-05-03 17:02:07 +0200

    Generación del histograma

--- docs/index.md
+++ docs/index.md
@@ -7,3 +7,16 @@
 ## Descripción
 
 En esta página se procede a mostrar algunos ejemplos de diferentes técnicas para visualizar datos.
+
+
+## Fuente de datos
+
+Para el presente trabajo académico, se ha procedido a utilizar un data set de evaluaciones de películas por género en IMDB [evaluaciones de películas por género en IMDB](https://zenodo.org/record/7339445/files/IMDB%20Selection%20Database.csv?download=1)
+
+
+## Histograma
+
+El primer ejemplo que vamos a visualizar, se trata de un histograma. Como los datos de puntuación de películas tienen una puntuación, vamos a representar el histograma de la puntuación de las mismas.
+
+[Histograma](./histogram.png)
+
---
+++ main.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np  
+import matplotlib.pyplot as plt  
+import plotly.express as px
+import base64
+
+CONST_HISTOGRAM_PATH = "docs/histogram.png"
+CONST_RADARCHAT_PATH = "docs/radarchat.png"
+CONST_SPARKLINE_PATH = "docs/sparkline.html"
+
+
+def calculate_mean(df, genre):
+
+    sum_value = 0
+    iterator = 0
+
+    for index, row in df.iterrows():
+        if genre in str(row['genres']):
+            sum_value += float(row['score'])
+            iterator += 1
+
+    return sum_value/iterator
+
+
+def generate_histogram(values):
+
+    plt.hist(x=values)
+    plt.ylabel('Puntuaciones')
+    plt.title('Histograma')
+    plt.savefig(CONST_HISTOGRAM_PATH)
+
+def generate_radar_chat(values, description_values):
+    data_frame = pd.DataFrame(dict(
+        r=values,
+        theta=description_values))
+    fig = px.line_polar(data_frame, r='r', theta='theta', line_close=True)
+    fig.write_image(CONST_RADARCHAT_PATH)
+
+def sparkline(data, figsize=(4, 0.25), **kwags):
+
+    data = list(data)
+
+    fig, ax = plt.subplots(1, 1, figsize=figsize, **kwags)
+    ax.plot(data)
+    for k,v in ax.spines.items():
+        v.set_visible(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    plt.plot(len(data) - 1, data[len(data) - 1], 'r.')
+
+    ax.fill_between(range(len(data)), data, len(data)*[min(data)], alpha=0.1)
+
+    img = BytesIO()
+    plt.savefig(img, transparent=True, bbox_inches='tight')
+    img.seek(0)
+    plt.close()
+
+    return base64.b64encode(img.read()).decode("UTF-8")
+
+def generate_sparkline(values):
+
+    with open(CONST_SPARKLINE_PATH, "w") as file:
+        for value in values:
+            file.write('<div><img src="data:image/png;base64,{}"/></div>'.format(sparkline(value)))
+
+if __name__ == "__main__":
+
+    url = "https://zenodo.org/record/7339445/files/IMDB%20Selection%20Database.csv?download=1"
+
+    dataSet = pd.read_csv(url)
+
+    genres_list =[genre.replace("'", "") for genre in set(
+        np.concatenate(
+        dataSet['genres'].apply(lambda x: x[1:-1].split(', ')).values))]
+
+    genre_means = list()
+
+    for genre in genres_list:
+        genre_means.append(calculate_mean(dataSet, genre))
+    
+    generate_histogram(dataSet['score'])
+    generate_radar_chat(genre_means, genres_list)
+
+
